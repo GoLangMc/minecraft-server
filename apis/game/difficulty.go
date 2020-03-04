ef60a5eaa221efc4b8d50a099970cb95b4b29809
Sxtanna <ranald.taylor@gmail.com>
2020-03-04 12:07:07 -0500

    updated difficulty to a byte, implemented Stringer, added IO functions

--- apis/game/difficulty.go
+++ apis/game/difficulty.go
@@ -1,6 +1,8 @@
 package game
 
-type Difficulty int
+import "fmt"
+
+type Difficulty byte
 
 const (
 	PEACEFUL Difficulty = iota
@@ -8,3 +10,37 @@ const (
 	NORMAL
 	HARD
 )
+
+func (d Difficulty) String() string {
+	switch d {
+	case PEACEFUL:
+		return "Peaceful"
+	case EASY:
+		return "Easy"
+	case NORMAL:
+		return "Normal"
+	case HARD:
+		return "Hard"
+	default:
+		panic(fmt.Errorf("no difficulty for id %d", byte(d)))
+	}
+}
+
+func ValueOfDifficulty(d Difficulty) byte {
+	return byte(d)
+}
+
+func DifficultyValueOf(id byte) Difficulty {
+	switch id {
+	case 0:
+		return PEACEFUL
+	case 1:
+		return EASY
+	case 2:
+		return NORMAL
+	case 3:
+		return HARD
+	default:
+		panic(fmt.Errorf("no difficulty for id %d", id))
+	}
+}
