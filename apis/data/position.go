c27730bfc62d665729ef992ab3b32e2de1261a1e
Sxtanna <ranald.taylor@gmail.com>
2020-03-04 00:22:25 -0500

    separated Position into PositionI and PositionF

--- apis/data/position.go
+++ apis/data/position.go
@@ -1,9 +1,19 @@
 package data
 
-type Position struct {
+type PositionI struct {
+	X int64
+	Y int64
+	Z int64
+}
+
+type PositionF struct {
 	X float64
 	Y float64
 	Z float64
+}
+
+type Position struct {
+	PositionF
 
 	Yaw   float32
 	Pitch float32
