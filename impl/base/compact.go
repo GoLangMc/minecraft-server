c29a6a206b7a3e22ade07e0a7099a639ea37bb4e
Sxtanna <ranald.taylor@gmail.com>
2020-03-06 22:50:19 -0500

    added Compacter for compact int64 storage

---
+++ impl/base/compact.go
@@ -0,0 +1,79 @@
+package base
+
+type Compacter struct {
+	Values []int64
+
+	bpb int
+	max int
+}
+
+func NewCompacter(bits, size int) *Compacter {
+	sliceSize := iDontKnowWhatThisDoes(size*bits, 64) / 64
+
+	return &Compacter{
+		bpb:    bits,
+		max:    (1 << bits) - 1,
+		Values: make([]int64, sliceSize, sliceSize),
+	}
+}
+
+func (c *Compacter) Set(index int, value int64) int64 {
+	bIndex := index * c.bpb
+
+	sIndex := bIndex >> 0x06
+	eIndex := (((index + 1) * c.bpb) - 1) >> 0x06
+
+	uIndex := bIndex ^ (sIndex << 0x06)
+
+	previousValue := int64(uint64(c.Values[sIndex])>>uIndex) & int64(c.max)
+
+	c.Values[sIndex] = c.Values[sIndex]&int64(^(c.max<<uIndex)) | ((value & int64(c.max)) << uIndex)
+
+	if sIndex != eIndex {
+		zIndex := 64 - uIndex
+		pIndex := c.bpb - 1
+
+		previousValue |= (c.Values[eIndex] << zIndex) & int64(c.max)
+
+		c.Values[eIndex] = int64(((uint64(c.Values[eIndex]) >> pIndex) << pIndex) | uint64((value&int64(c.max))>>zIndex))
+	}
+
+	return previousValue
+}
+
+func (c *Compacter) Get(index int) int64 {
+	bIndex := index * c.bpb
+
+	sIndex := bIndex >> 0x06
+	eIndex := (((index + 1) * c.bpb) - 1) >> 0x06
+
+	uIndex := bIndex ^ (sIndex << 0x06)
+
+	if sIndex == eIndex {
+		return int64((uint64(c.Values[sIndex]) >> uIndex) & uint64(c.max))
+	}
+
+	zIndex := 64 - uIndex
+
+	return int64(uint64(c.Values[sIndex]>>uIndex) | uint64(c.Values[eIndex]<<zIndex)&uint64(c.max))
+}
+
+func iDontKnowWhatThisDoes(var0, var1 int) int {
+	if var1 == 0 {
+		return 0
+	} else if var0 == 0 {
+		return var1
+	} else {
+		if var0 < 0 {
+			var1 *= -1
+		}
+
+		var2 := var0 % var1
+
+		if var2 == 0 {
+			return var0
+		} else {
+			return var0 + var1 - var2
+		}
+	}
+}
