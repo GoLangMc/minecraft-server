b29b20d4ab74f81b70a039a3d0541859a447d913
Sxtanna <ranald.taylor@gmail.com>
2020-03-06 22:51:10 -0500

    updated slice implementation to use Compacter

--- impl/game/level/slice.go
+++ impl/game/level/slice.go
@@ -10,7 +10,7 @@ type slice struct {
 
 	chunk *chunk
 
-	values []int64
+	values *base.Compacter
 }
 
 func newSlice(chunk *chunk, index int) *slice {
@@ -19,7 +19,7 @@ func newSlice(chunk *chunk, index int) *slice {
 
 		chunk: chunk,
 
-		values: make([]int64, apis_level.SliceS, apis_level.SliceS),
+		values: base.NewCompacter(apis_level.BitsPerBlock, apis_level.SliceS),
 	}
 
 	return slice
@@ -64,52 +64,19 @@ func (s *slice) Push(writer base.Buffer) {
 
 	// the server is using the direct palette
 
-	writer.PushVrI(int32(len(s.values)))
+	writer.PushVrI(int32(len(s.values.Values)))
 
-	for _, value := range s.values {
+	for _, value := range s.values.Values {
 		writer.PushI64(value)
 	}
 }
 
 func (s *slice) sliceBlockGet(index int) int64 {
-	bIndex := index * apis_level.BitsPerBlock
-
-	sIndex := bIndex >> 0x06
-	eIndex := (((index + 1) * apis_level.BitsPerBlock) - 1) >> 0x06
-
-	uIndex := bIndex ^ (sIndex << 0x06)
-
-	if sIndex == eIndex {
-		return int64((uint64(s.values[sIndex]) >> uIndex) & apis_level.MaxPaletteID)
-	}
-
-	zIndex := 64 - uIndex
-
-	return int64(uint64(s.values[sIndex]>>uIndex) | uint64((s.values[eIndex]<<zIndex))&apis_level.MaxPaletteID)
+	return s.values.Get(index)
 }
 
 func (s *slice) sliceBlockSet(index int, value int64) int64 {
-	bIndex := index * apis_level.BitsPerBlock
-
-	sIndex := bIndex >> 0x06
-	eIndex := (((index + 1) * apis_level.BitsPerBlock) - 1) >> 0x06
-
-	uIndex := bIndex ^ (sIndex << 0x06)
-
-	previousValue := int64((uint64(s.values[sIndex]) >> uIndex) & apis_level.MaxPaletteID)
-
-	s.values[sIndex] = (s.values[sIndex] & ^(apis_level.MaxPaletteID << uIndex)) | ((value & apis_level.MaxPaletteID) << uIndex)
-
-	if sIndex != eIndex {
-		zIndex := 64 - uIndex
-		pIndex := apis_level.BitsPerBlock - 1
-
-		previousValue |= (s.values[eIndex] << zIndex) & apis_level.MaxPaletteID
-
-		s.values[eIndex] = int64(((uint64(s.values[eIndex]) >> pIndex) << pIndex) | uint64((value&apis_level.MaxPaletteID)>>zIndex))
-	}
-
-	return previousValue
+	return s.values.Set(index, value)
 }
 
 func (s *slice) fill(value int64) {
